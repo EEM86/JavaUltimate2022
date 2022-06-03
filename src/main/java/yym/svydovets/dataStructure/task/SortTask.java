6d72fd4e3cbf68edde30437d3c4f4b1361851260
YY <eevgeniy@gmail.com>
2022-06-03 20:03:16 +0300

    Add merge task

--- src/main/java/yym/svydovets/dataStructure/task/SortTask.java
+++ src/main/java/yym/svydovets/dataStructure/task/SortTask.java
@@ -4,15 +4,22 @@ import yym.svydovets.utils.ArrGen;
 
 import java.util.Arrays;
 
-public class InsertionSortTask {
+public class SortTask {
 
   public static void main(String[] args) {
     final int[] array = ArrGen.generate(10, 100);
     System.out.print(Arrays.toString(array));
 //    insertionSort(array);
-    bubbleSort(array);
+//    bubbleSort(array);
+//    mergeSort(array);
     System.out.println();
     System.out.print(Arrays.toString(array));
+  }
+
+  private static void mergeSort(int[] array) {
+  }
+
+  private static void merge(int[] array, int[] left, int[] right) {
   }
 
   private static void bubbleSort(int[] array) {
