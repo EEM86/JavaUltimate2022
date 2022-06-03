22abcb81cd7be087b56f81e34021d65e61f6beed
YY <eevgeniy@gmail.com>
2022-06-03 20:04:56 +0300

    Add heterogenous holder task

---
+++ src/main/java/yym/svydovets/dataStructure/task/HeterojeneousMaxHolder.java
@@ -0,0 +1,18 @@
+package yym.svydovets.dataStructure.task;
+
+public class HeterojeneousMaxHolder {
+  // ToDo Implement a multi-type container that holds maximum values.
+  //  It should store up to one value per type, should be type-safe
+
+  public static void main(String[] args) {
+    final HeterojeneousMaxHolder map = new HeterojeneousMaxHolder();
+//    map.put(Integer.class, 3);
+//    map.put(Integer.class, 9);
+//    map.put(Integer.class, 6);
+//    map.put(String.class, "6");
+//    map.put(String.class, "8");
+//    map.put(String.class, "2");
+//    System.out.println(map.getMax(Integer.class));
+//    System.out.print(map.getMax(String.class));
+  }
+}
