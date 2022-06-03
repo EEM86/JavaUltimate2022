abc7d97ccdce337ada5bc0528e7d28e62e48ceb2
YY <eevgeniy@gmail.com>
2022-06-03 12:53:56 +0300

    add merge sort solution

--- src/main/java/yym/svydovets/dataStructure/task/SortTask.java
+++ src/main/java/yym/svydovets/dataStructure/task/SortTask.java
@@ -4,15 +4,48 @@ import yym.svydovets.utils.ArrGen;
 
 import java.util.Arrays;
 
-public class InsertionSortTask {
+public class SortTask {
 
   public static void main(String[] args) {
     final int[] array = ArrGen.generate(10, 100);
     System.out.print(Arrays.toString(array));
 //    insertionSort(array);
-    bubbleSort(array);
+//    bubbleSort(array);
+    mergeSort(array);
     System.out.println();
     System.out.print(Arrays.toString(array));
+  }
+
+  private static void mergeSort(int[] array) {
+    final int n = array.length;
+
+    if (n <= 1) {
+      return;
+    }
+
+    var mid = n / 2;
+    int[] left = Arrays.copyOfRange(array, 0, mid);
+    int[] right = Arrays.copyOfRange(array, mid, n);
+
+    mergeSort(left);
+    mergeSort(right);
+
+    merge(array, left, right);
+  }
+
+  private static void merge(int[] array, int[] left, int[] right) {
+    int leftIdx = 0, rightIdx = 0;
+
+    while (leftIdx < left.length && rightIdx < right.length) {
+      if (left[leftIdx] < right[rightIdx]) {
+        array[leftIdx + rightIdx] = left[leftIdx++];
+      } else {
+        array[leftIdx + rightIdx] = right[rightIdx++];
+      }
+    }
+
+    System.arraycopy(left, leftIdx, array, leftIdx + rightIdx, left.length - leftIdx);
+    System.arraycopy(right, rightIdx, array, leftIdx + rightIdx, right.length - rightIdx);
   }
 
   private static void bubbleSort(int[] array) {
