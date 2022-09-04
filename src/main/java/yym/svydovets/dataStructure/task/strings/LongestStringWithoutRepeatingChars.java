6281dba3daeb7e2a29ecc78226f5183fa156e196
YY <eevgeniy@gmail.com>
2022-09-04 19:18:52 +0300

    Add 4 medium 1 easy tasks from leetcode

---
+++ src/main/java/yym/svydovets/dataStructure/task/strings/LongestStringWithoutRepeatingChars.java
@@ -0,0 +1,28 @@
+package yym.svydovets.dataStructure.task.strings;
+
+import org.apache.commons.lang3.NotImplementedException;
+
+import java.util.HashSet;
+
+public class LongestStringWithoutRepeatingChars {
+
+  public static void main(String[] args) {
+    var s = "pwwkew";
+    System.out.println(lengthOfLongestSubstring(s));
+  }
+
+  /*
+  Given a string s, find the length of the longest substring without repeating characters.
+  Example 1:
+  Input: s = "abcabcbb"
+  Output: 3
+  Explanation: The answer is "abc", with the length of 3.
+  https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+  WINDOW solution
+   */
+  static int lengthOfLongestSubstring(String s) {
+    throw new NotImplementedException();
+  }
+
+}
