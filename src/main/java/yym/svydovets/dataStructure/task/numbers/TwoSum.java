e9bebb85657bc7d67896a18df81c98a3c3ea56d2
YY <eevgeniy@gmail.com>
2022-09-04 09:10:36 +0300

    Add 2 medium tasks from leetcode

--- src/main/java/yym/svydovets/dataStructure/task/numbers/TwoSum.java
+++ src/main/java/yym/svydovets/dataStructure/task/numbers/TwoSum.java
@@ -2,16 +2,23 @@ package yym.svydovets.dataStructure.task.numbers;
 
 import org.apache.commons.lang3.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class TwoSum {
 
   public static void main(String[] args) {
-    var nums = new int[] {2, 7, 11, 15};
-    var target = 9;
-    System.out.println(Arrays.toString(twoSum(nums, target)));
+//    var nums = new int[] {2,3,11,15};
+//    var target = 9;
+//    System.out.println(Arrays.toString(twoSumV2(nums, target)));
+
+    var threeNums = new int[]{-1,0,1,2,-1,-4};
+    threeSum(threeNums).forEach(System.out::println);
+
   }
 
   /*
@@ -26,6 +33,44 @@ public class TwoSum {
   https://leetcode.com/problems/two-sum/submissions/
    */
   static int[] twoSum(int[] nums, int target) {
+    throw new NotImplementedException();
+  }
+
+  /*
+  Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
+  find two numbers such that they add up to a specific target number.
+  Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+  Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+  The tests are generated such that there is exactly one solution. You may not use the same element twice.
+  Your solution must use only constant extra space.
+
+  Example 1:
+  Input: numbers = [2,7,11,15], target = 9
+  Output: [1,2]
+  Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+  https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+   */
+  static int[] twoSumV2(int[] numbers, int target) {
+    throw new NotImplementedException();
+  }
+
+
+
+  /*
+  Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+  Notice that the solution set must not contain duplicate triplets.
+  Example:
+  Input: nums = [-1,0,1,2,-1,-4]
+  Output: [[-1,-1,2],[-1,0,1]]
+  Explanation:
+  nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+  nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+  nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+  The distinct triplets are [-1,0,1] and [-1,-1,2].
+  Notice that the order of the output and the order of the triplets does not matter.
+  https://leetcode.com/problems/3sum/
+   */
+  static List<List<Integer>> threeSum(int[] nums) {
     throw new NotImplementedException();
   }
 
