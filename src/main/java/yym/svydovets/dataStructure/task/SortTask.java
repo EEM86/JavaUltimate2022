d9de22eb4a62fa8c4fab33717ccdb588d78f5eaa
YY <eevgeniy@gmail.com>
2023-03-18 12:35:53 +0200

    add tests for SortTask

--- src/main/java/yym/svydovets/dataStructure/task/SortTask.java
+++ src/main/java/yym/svydovets/dataStructure/task/SortTask.java
@@ -1,55 +1,28 @@
 package yym.svydovets.dataStructure.task;
 
-import yym.svydovets.utils.ArrGen;
-
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
 public class SortTask {
 
-  public static void main(String[] args) {
-    final int[] array = ArrGen.generate(10, 100);
-    System.out.print(Arrays.toString(array));
-//    insertionSort(array);
-//    bubbleSort(array);
-//    mergeSort(array);
-    System.out.println();
-    System.out.print(Arrays.toString(array));
-
-    //    checkMergeList();
-  }
-
   // ToDo implement mergeSort
-  private static void mergeSort(int[] array) {
+  public static void mergeSort(int[] array) {
 
   }
 
   // ToDo implement bubbleSort
-  private static void bubbleSort(int[] array) {
+  public static void bubbleSort(int[] array) {
 
   }
 
   // ToDo implement insertionSort
-  private static void insertionSort(int[] array) {
+  public static void insertionSort(int[] array) {
 
   }
 
   //ToDo implement mergeSort with generics List
-  private static <T extends Comparable<? super T>> void mergeSort(List<T> list, Comparator<? super T> comparator) {
+  public static <T extends Comparable<? super T>> void mergeSort(List<T> list, Comparator<? super T> comparator) {
 
-  }
-
-  private static void checkMergeList() {
-    var list = ArrGen.generateList(10, 40);
-
-    System.out.println();
-    list.forEach(o -> System.out.print(o + " "));
-    System.out.println();
-
-    mergeSort(list, Comparator.naturalOrder());
-
-    list.forEach(o -> System.out.print(o + " "));
   }
 
 }
---
+++ src/test/java/yym/svydovets/dataStructure/task/SortTaskTest.java
@@ -0,0 +1,59 @@
+package yym.svydovets.dataStructure.task;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SortTaskTest {
+
+  @Test
+  public void testMergeSort() {
+    int[] unsortedArray = {4, 2, 8, 1, 3, 7};
+    int[] sortedArray = {1, 2, 3, 4, 7, 8};
+
+    SortTask.mergeSort(unsortedArray);
+
+    assertArrayEquals(sortedArray, unsortedArray);
+  }
+
+  @Test
+  public void testBubbleSort() {
+    int[] unsortedArray = {4, 2, 8, 1, 3, 7};
+    int[] sortedArray = {1, 2, 3, 4, 7, 8};
+
+    SortTask.bubbleSort(unsortedArray);
+
+    assertArrayEquals(sortedArray, unsortedArray);
+  }
+
+  @Test
+  public void testInsertionSort() {
+    int[] unsortedArray = {4, 2, 8, 1, 3, 7};
+    int[] sortedArray = {1, 2, 3, 4, 7, 8};
+
+    SortTask.insertionSort(unsortedArray);
+
+    assertArrayEquals(sortedArray, unsortedArray);
+  }
+
+  @Test
+  public void testMergeListSort() {
+    // create an ArrayList of unsorted integers
+    ArrayList<Integer> unsortedList = new ArrayList<>(Arrays.asList(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5));
+
+    // sort the ArrayList in ascending order
+    SortTask.mergeSort(unsortedList, Comparator.naturalOrder());
+
+    // create an ArrayList of the expected sorted integers
+    ArrayList<Integer> expectedList = new ArrayList<>(Arrays.asList(1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9));
+
+    // assert that the sorted ArrayList matches the expected sorted ArrayList
+    assertEquals(expectedList, unsortedList);
+  }
+
+}
