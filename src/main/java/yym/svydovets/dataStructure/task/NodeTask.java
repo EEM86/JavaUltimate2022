6a63e3260bb2e7d6f5c1003941294ad88d594a8f
YY <eevgeniy@gmail.com>
2023-03-19 11:07:13 +0200

    add NodeTask

---
+++ src/main/java/yym/svydovets/dataStructure/task/NodeTask.java
@@ -0,0 +1,43 @@
+package yym.svydovets.dataStructure.task;
+
+import org.apache.commons.lang3.NotImplementedException;
+
+import java.util.Objects;
+
+/**
+ * Create a node list and a reversed list
+ */
+public class NodeTask {
+
+  public static void main(String[] args) {
+    var node = Node.of(4, 5, 7, 9, 11);
+    node = node.reverse();
+    node.printList();
+  }
+
+
+  private static class Node<T> {
+
+    T value;
+    Node<T> next;
+
+    public Node(T value) {
+      this.value = value;
+    }
+
+    public static <E> Node<E> of(E... values) {
+      throw new NotImplementedException();
+    }
+
+    public void printList() {
+
+    }
+
+    public <E> Node<T> reverse() {
+      throw new NotImplementedException();
+    }
+
+  }
+}
+
+
