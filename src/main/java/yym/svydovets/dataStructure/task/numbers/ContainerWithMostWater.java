90186d031ee05c7055486b85f9c3ff2b1448b97d
YY <eevgeniy@gmail.com>
2023-03-23 09:40:05 +0200

    add test for ContainerWithMostWater

--- src/main/java/yym/svydovets/dataStructure/task/numbers/ContainerWithMostWater.java
+++ src/main/java/yym/svydovets/dataStructure/task/numbers/ContainerWithMostWater.java
@@ -4,12 +4,8 @@ import org.apache.commons.lang3.NotImplementedException;
 
 public class ContainerWithMostWater {
 
-  public static void main(String[] args) {
-    var height = new int[] {1, 8, 6, 2, 5, 4, 8, 3, 7};
-    System.out.println(maxArea(height));
-  }
-
   /*
+  Medium
   You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints
   of the ith line are (i, 0) and (i, height[i]).
   Find two lines that together with the x-axis form a container, such that the container contains the most water.
