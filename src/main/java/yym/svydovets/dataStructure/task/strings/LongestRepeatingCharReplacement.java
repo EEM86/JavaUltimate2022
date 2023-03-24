989ef95b942a33a18e27a70d3c8dfe0245cec3b5
YY <eevgeniy@gmail.com>
2023-03-24 14:58:59 +0200

    add test for LongestRepeatingCharReplacement

--- src/main/java/yym/svydovets/dataStructure/task/strings/LongestRepeatingCharReplacement.java
+++ src/main/java/yym/svydovets/dataStructure/task/strings/LongestRepeatingCharReplacement.java
@@ -4,17 +4,13 @@ import org.apache.commons.lang3.NotImplementedException;
 
 public class LongestRepeatingCharReplacement {
 
-  public static void main(String[] args) {
-    var s = "AABABBA";
-    var k = 1;
-    System.out.println(characterReplacement(s, k));
-  }
-
   /*
-  You are given a string s and an integer k. You can choose any character of the string and change
-  it to any other uppercase English character.
+  Medium
+  You are given a string s and an integer k. You can choose any character of the string
+  and change it to any other uppercase English character.
   You can perform this operation at most k times.
-  Return the length of the longest substring containing the same letter you can get after performing the above operations.
+  Return the length of the longest substring containing the same letter
+  you can get after performing the above operations.
   Example 1:
   Input: s = "ABAB", k = 2 ; Input: s = "AABABBA", k = 1
   Output: 4                ; 4
---
+++ src/test/java/yym/svydovets/dataStructure/task/strings/LongestRepeatingCharReplacementTest.java
@@ -0,0 +1,34 @@
+package yym.svydovets.dataStructure.task.strings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static yym.svydovets.dataStructure.task.strings.LongestRepeatingCharReplacement.characterReplacement;
+
+class LongestRepeatingCharReplacementTest {
+
+  @Test
+  void testCharacterReplacement_v1() {
+    String input = "ABAB";
+    int changesAmount = 2;
+
+    int expected = 4;
+
+    int actual = characterReplacement(input, changesAmount);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void testCharacterReplacement_v2() {
+    String input = "AABABBA";
+    int changesAmount = 1;
+
+    int expected = 4;
+
+    int actual = characterReplacement(input, changesAmount);
+
+    assertEquals(expected, actual);
+  }
+
+}
