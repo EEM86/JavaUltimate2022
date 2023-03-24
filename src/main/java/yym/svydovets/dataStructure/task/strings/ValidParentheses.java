b8204afc9468cf2b6de2f975bcabccbfb3a3c2b1
YY <eevgeniy@gmail.com>
2023-03-24 16:12:23 +0200

    add test for ValidParentheses

--- src/main/java/yym/svydovets/dataStructure/task/strings/ValidParentheses.java
+++ src/main/java/yym/svydovets/dataStructure/task/strings/ValidParentheses.java
@@ -7,11 +7,17 @@ import java.util.Stack;
 
 public class ValidParentheses {
 
-  public static void main(String[] args) {
-    var s = "((";
-    System.out.println(isValid(s));
-  }
+  /*
+  Easy
+  * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+  * determine if the input string is valid.
 
+  * An input string is valid if:
+  * Open brackets must be closed by the same type of brackets.
+  * Open brackets must be closed in the correct order.
+  * Every close bracket has a corresponding open bracket of the same type.
+
+   */
   static boolean isValid(String s) {
     throw new NotImplementedException();
   }
