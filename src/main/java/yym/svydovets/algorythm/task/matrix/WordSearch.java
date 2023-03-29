fc887ed6e70004161fe09b747ab0d3fd4f8bba60
YY <eevgeniy@gmail.com>
2023-03-29 15:57:32 +0300

    add test WordSearch

--- src/main/java/yym/svydovets/algorythm/task/matrix/WordSearch.java
+++ src/main/java/yym/svydovets/algorythm/task/matrix/WordSearch.java
@@ -4,17 +4,8 @@ import org.apache.commons.lang3.NotImplementedException;
 
 public class WordSearch {
 
-    public static void main(String[] args) {
-        char[][] board = new char[][]{
-            {'A','B','C','E'},
-            {'S','F','C','S'},
-            {'A','D','E','E'}
-        };
-        var word = "ABCCED";
-        System.out.println(exist(board, word));
-    }
-
     /*
+    Medium
     Given an m x n grid of characters board and a string word, return true if word exists in the grid.
 
     The word can be constructed from letters of sequentially adjacent cells,
