59aaea45c1dcf53637bad39a0468a4b2f6e87f74
YY <eevgeniy@gmail.com>
2023-03-29 15:42:06 +0300

    add test for NumsOfIslands

--- src/main/java/yym/svydovets/algorythm/task/matrix/NumsOfIslands.java
+++ src/main/java/yym/svydovets/algorythm/task/matrix/NumsOfIslands.java
@@ -10,19 +10,14 @@ import org.apache.commons.lang3.NotImplementedException;
 public class NumsOfIslands {
 
   public static void main(String[] args) {
-    var grid = new char[][]{
-        {'1','1','1','1','0'},
-        {'1','1','0','1','0'},
-        {'1','1','0','0','0'},
-        {'0','0','0','0','0'}
-    };
 
-    System.out.println(numIslands(grid));
   }
-
   /*
-  Given an m x n 2D binary grid which represents a map of '1's (land) and '0's (water), return the number of islands.
-  An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+  Medium
+  Given an m x n 2D binary grid which represents a map of
+  '1's (land) and '0's (water), return the number of islands.
+  An island is surrounded by water and is formed by connecting
+  adjacent lands horizontally or vertically.
   You may assume all four edges of the grid are all surrounded by water.
   Example 1:
   Input: grid = [
@@ -32,15 +27,7 @@ public class NumsOfIslands {
     {'0','0','0','0','0'}
   ]
   Output: 1
-  +++++++++++++++++++++++++++++++++++++
-  Example 2:
-  Input: grid = [
-    {'1','1','0','0','0'},
-    {'1','1','0','0','0'},
-    {'0','0','1','0','0'},
-    {'0','0','0','1','1'}
-  ]
-  Output: 3
+  https://leetcode.com/problems/number-of-islands/
    */
   static int numIslands(char[][] grid) {
     throw new NotImplementedException();
