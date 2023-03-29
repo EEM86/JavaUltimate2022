403e7297f47121d63adc058df3376b7d92de8b9c
YY <eevgeniy@gmail.com>
2023-03-29 17:33:32 +0300

    add solution for ValidSudoku

--- src/main/java/yym/svydovets/algorythm/task/matrix/ValidSudoku.java
+++ src/main/java/yym/svydovets/algorythm/task/matrix/ValidSudoku.java
@@ -88,88 +88,20 @@ public class ValidSudoku {
   }
 
   static boolean isValidSudokuSite(char[][] board) {
-    int rows = board.length;
-    int cols = board[0].length;
-
-    Set<Character> rowSet = null;
-    Set<Character> colSet = null;
-
-    //check for rows
-    for(int i = 0; i<rows; i++){
-      rowSet = new HashSet<>();
-      for(int j = 0; j<cols; j++){
-        if(board[i][j] == '.'){
-          continue;
-        }
-        if(rowSet.contains(board[i][j])){
-          return false;
-        }
-        rowSet.add(board[i][j]);
-      }
-    }
-
-    //check for cols
-    for(int i = 0; i<cols; i++){
-      colSet = new HashSet<>();
-      for(int j = 0; j<rows; j++){
-        if(board[j][i] == '.'){
-          continue;
-        }
-        if(colSet.contains(board[j][i])){
-          return false;
-        }
-
-        colSet.add(board[j][i]);
-      }
-    }
-
-    //block
-    for(int i = 0; i<rows; i=i+3){
-      for(int j = 0; j<cols; j=j+3){
-        if(!checkBlock(i,j,board)){
-          return false;
+    var numbers = new HashSet<String>();
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        var currentChar = board[i][j];
+        if (currentChar != '.') {
+          if (!numbers.add(currentChar + " row " + i) ||
+              !numbers.add(currentChar + " column " + j) ||
+              !numbers.add(currentChar + " block " + i/3 + "-" + j/3)) {
+            return false;
+          }
         }
       }
     }
-
     return true;
   }
-
-  static boolean checkBlock(int idxI, int idxJ, char[][] board){
-    Set<Character> blockSet = new HashSet<>();
-    int rows = idxI + 3;
-    int cols = idxJ + 3;
-    for(int i = idxI; i<rows; i++){
-      for(int j = idxJ; j < cols; j++){
-        if(board[i][j] == '.'){
-          continue;
-        }
-
-        if(blockSet.contains(board[i][j])){
-          return false;
-        }
-
-        blockSet.add(board[i][j]);
-      }
-    }
-
-    return true;
-  }
-
-
-  static class Pair {
-    public int left;
-    public int right;
-
-    public Pair() {
-
-    };
-
-    public Pair(int left, int right) {
-      this.left = left;
-      this.right = right;
-    }
-  }
-
 
 }
