28fb4ee02912ea4b76b0bfc00b4426f0b48e78a8
YY <eevgeniy@gmail.com>
2023-04-23 10:29:08 +0300

    add test for product of array

--- src/test/java/yym/svydovets/algorythm/task/numbers/ProductOfArrayExceptSelfTest.java
+++ src/test/java/yym/svydovets/algorythm/task/numbers/ProductOfArrayExceptSelfTest.java
@@ -8,9 +8,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProductOfArrayExceptSelfTest {
 
   @Test
-  void testProductExceptSelf() {
+  void testProductExceptSelf_v1() {
     int[] input = new int[]{1,2,3,4};
     int[] expected = new int[]{24,12,8,6};
+
+    int[] actual = ProductOfArrayExceptSelf.productExceptSelf(input);
+
+    assertArrayEquals(expected, actual);
+  }
+
+  @Test
+  void testProductExceptSelf_v2() {
+    int[] input = new int[]{-1,1,0,-3,3};
+    int[] expected = new int[]{0,0,9,0,0};
 
     int[] actual = ProductOfArrayExceptSelf.productExceptSelf(input);
 
