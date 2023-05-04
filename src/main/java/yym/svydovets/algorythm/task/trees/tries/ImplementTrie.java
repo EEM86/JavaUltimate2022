bd138757dfc84fa8130b367b50434b0694b9ee6d
YY <eevgeniy@gmail.com>
2023-05-04 20:18:30 +0300

    add trie section with explanation
    Readme.md explains what is a trie with examples of usages.

---
+++ src/main/java/yym/svydovets/algorythm/task/trees/tries/ImplementTrie.java
@@ -0,0 +1,30 @@
+package yym.svydovets.algorythm.task.trees.tries;
+
+public class ImplementTrie {
+
+  /*
+   * Implement Trie.
+   * Input:
+   * ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+   * [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+   * Output: [null, null, true, false, true, null, true]
+   *
+   * Explanation
+   * Trie trie = new Trie();
+   * trie.insert("apple");
+   * trie.search("apple");   // return True
+   * trie.search("app");     // return False
+   * trie.startsWith("app"); // return True
+   * trie.insert("app");
+   * trie.search("app");     // return True
+   */
+  public static void main(String[] args) {
+    /*
+     * Your Trie object will be instantiated and called as such:
+     * Trie obj = new Trie();
+     * obj.insert(word);
+     * boolean param_2 = obj.search(word);
+     * boolean param_3 = obj.startsWith(prefix);
+     */
+  }
+}
