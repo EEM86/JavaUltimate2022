75196b6e5771935e1ece6f598f7e6839de5d4b32
YY <eevgeniy@gmail.com>
2023-05-12 21:40:13 +0300

    add WordDictionary test

--- src/test/java/yym/svydovets/algorythm/task/trees/tries/WordDictionaryTest.java
+++ src/test/java/yym/svydovets/algorythm/task/trees/tries/WordDictionaryTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class WordDictionaryTest {
 
   @Test
-  void testWordDictionary() {
+  void testWordDictionary_v1() {
     var dict = new WordDictionary();
     dict.addWord("bad");
     dict.addWord("dad");
@@ -19,4 +19,24 @@ class WordDictionaryTest {
     assertTrue(dict.search("b.."));
   }
 
+  @Test
+  void testWordDictionary_v2() {
+    var dict = new WordDictionary();
+    dict.addWord("at");
+    dict.addWord("and");
+    dict.addWord("an");
+    dict.addWord("add");
+
+    assertFalse(dict.search("a"));
+    assertFalse(dict.search(".at"));
+
+    dict.addWord("bat");
+
+    assertTrue(dict.search(".at"));
+    assertTrue(dict.search("an."));
+    assertFalse(dict.search("a.d."));
+    assertFalse(dict.search("b."));
+    assertTrue(dict.search("a.d"));
+    assertFalse(dict.search("."));
+  }
 }
