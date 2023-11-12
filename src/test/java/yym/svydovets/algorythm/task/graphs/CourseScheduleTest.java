0e36f448580dc60236a11f2806ac1992f58a67c0
YY <eevgeniy@gmail.com>
2023-11-12 17:32:59 +0200

    add graph course schedule task

---
+++ src/test/java/yym/svydovets/algorythm/task/graphs/CourseScheduleTest.java
@@ -0,0 +1,36 @@
+package yym.svydovets.algorythm.task.graphs;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseScheduleTest {
+
+  CourseSchedule service;
+
+  @BeforeEach
+  void setUp() {
+    service = new CourseSchedule();
+  }
+
+  @Test
+  void canFinish_v1() {
+    int numCourses = 2;
+    int[][] prerequisites = { {1, 0} };
+
+    final boolean result = service.canFinish(numCourses, prerequisites);
+
+    assertTrue(result);
+  }
+
+  @Test
+  void canFinish_v2() {
+    int numCourses = 2;
+    int[][] prerequisites = { {1, 0}, {0, 1} };
+
+    final boolean result = service.canFinish(numCourses, prerequisites);
+
+    assertFalse(result);
+  }
+}
