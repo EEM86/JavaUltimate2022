8953e435b1b02abc2f5cca429c486b1e788a222f
YY <eevgeniy@gmail.com>
2023-11-15 13:10:33 +0200

    add hard graph task alien dictionary

---
+++ src/main/java/yym/svydovets/algorithm/task/graphs/AlienDictionary.java
@@ -0,0 +1,27 @@
+package yym.svydovets.algorithm.task.graphs;
+
+import org.apache.commons.lang3.NotImplementedException;
+import yym.svydovets.utils.Tuple;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class AlienDictionary {
+
+  /* Hard
+   * You are given a list of strings 'words' sorted lexicographically
+   * by the rules of alien's language.
+   * Return a sorted string of unique letters,
+   * if there is no solution, return "", if multiple - any of them.
+   *
+   * Example:
+   * Input: words = ["wrt", "wrf", "er", "ett", "rftt"]
+   * Output: "wertf"
+   */
+  public String alienOrder(String[] words) {
+    throw new NotImplementedException();
+  }
+
+}
